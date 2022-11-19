d5ad1f5714db5357f23af2904571a508e58ec55f
Takako <takakoyamaguchi.0204@gmail.com>
2022-11-19 04:04:20 +0000

    modelsファイルに属性追加

--- app/models/student.rb
+++ app/models/student.rb
@@ -1,2 +1,12 @@
 class Student < ApplicationRecord
+    # 苗字
+    validates :first_name, presence: true
+    # 名前
+    validates :last_name, presence: true
+    # 年齢
+    validates :age, presence: true
+    # 生まれた場所
+    validates :place_of_birth, presence: true
+    # 好きな教科
+    validates :like_subject, presence: true
 end
