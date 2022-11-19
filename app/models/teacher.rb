06e070d51f0c2319459f4dca354dc4802336379f
hayato <ha8to.f513@gmail.com>
2022-11-19 02:35:14 +0000

    日本語の追加

--- app/models/teacher.rb
+++ app/models/teacher.rb
@@ -1,7 +1,12 @@
 class Teacher < ApplicationRecord
+    # 苗字
     validates :first_name, presence: true
+    # 名前
     validates :last_name, presence: true
+    # 年齢
     validates :age, presence: true
+    # 出身地
     validates :place_of_birth, presence: true
+    # 好きな教科
     validates :like_subject, presence: true
 end
